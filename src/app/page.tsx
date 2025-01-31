0e562de17e046a903b67c0d8189654163fae1f36
Deva Mahayana <devamahayanatop@gmail.com>
2025-01-31 13:26:53 +0800

    Update try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
-
+import Contact from "@/pages/contact";
 export default function Home() {
   return (
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <label>
-        Name:
-        <input type="text" name="name" required />
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" required />
-      </label>
-      <button type="submit">Send</button>
-    </form>
-
+    <>
+    <Contact />
+    </>
   );
 }
---
+++ src/pages/contact.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const contact = () => {
+    return (
+        <div>
+          <h1>Contact Us</h1>
+          <form name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
+            <label>
+              Name:
+              <input type="text" name="name" required />
+            </label>
+            <label>
+              Email:
+              <input type="email" name="email" required />
+            </label>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+    );
+}
+
+export default contact
