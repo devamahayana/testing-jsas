880c4f8c3327b33ce485b9a2237a139181e1c654
Deva Mahayana <devamahayanatop@gmail.com>
2025-02-04 15:22:27 +0800

    das

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
+  reactStrictMode: true, // Keep strict mode enabled
+  experimental: {}, // Ensure there are no unnecessary keys
+  output: 'export',
 };
-
 
 export default nextConfig;
