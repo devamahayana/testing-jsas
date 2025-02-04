dc24caed3c40499af2907403e7fc34ff4084b2e1
Deva Mahayana <devamahayanatop@gmail.com>
2025-02-04 09:25:52 +0800

    Update form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,38 @@
 // pages/contact.js
 export default function Contact() {
+  const style = {
+    marginTop:{
+      marginTop: '20px'
+    },
+    textBlack:{
+      color: 'black'
+    }
+  }
     return (
       <div>
         <h1>Contact Us</h1>
-        <form name="contact" method="POST" data-netlify="true">
+        <form 
+          name="contact" method="POST" 
+          data-netlify="true"
+          data-netlify-honeypot="bot-field">
+            
           <input type="hidden" name="form-name" value="contact" />
-          <div>
+          <div style={style.marginTop}>
             <label>
-              Name: <input type="text" name="name" />
+              Name: <input type="text" name="name" style={style.textBlack}/>
             </label>
           </div>
-          <div>
+          <div style={style.marginTop}>
             <label>
-              Email: <input type="email" name="email" />
+              Email: <input type="email" name="email" style={style.textBlack}/>
             </label>
           </div>
-          <div>
+          <div style={style.marginTop}>
             <label>
-              Message: <textarea name="message"></textarea>
+              Message: <textarea name="message" style={style.textBlack}></textarea>
             </label>
           </div>
-          <div>
+          <div style={style.marginTop}>
             <button type="submit">Send</button>
           </div>
         </form>
