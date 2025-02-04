836be68ed6b64da94d5f62db3766b8206a548ae5
Deva Mahayana <devamahayanatop@gmail.com>
2025-02-04 10:23:55 +0800

    dasda

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to My Next.js App Erik Cahya</h1>
+      <h1>Welcome to My Form Next JS</h1>
       <Contact />
     </div>
   );
